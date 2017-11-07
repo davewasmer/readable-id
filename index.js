af34653f039727fcd6dfd5923122331dae74f192
Andrey Sitnik <andrey@sitnik.ru>
2017-11-07 19:16:59 +0300

    Replace shortid to nanoid (#2)

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-shortid = require('shortid')
-adjectives = require('./adjectives')
-nouns = require('./nouns')
+var nanoid = require('nanoid')
+var adjectives = require('./adjectives')
+var nouns = require('./nouns')
 
 // adjectives and nouns from https://gist.github.com/afriggeri/1266756
 
 module.exports = function() {
-  id = shortid.generate()
-  adjectiveIndex = Math.floor(Math.random() * adjectives.length)
-  nounIndex = Math.floor(Math.random() * nouns.length)
+  var id = nanoid(7)
+  var adjectiveIndex = Math.floor(Math.random() * adjectives.length)
+  var nounIndex = Math.floor(Math.random() * nouns.length)
   return adjectives[adjectiveIndex] + "-" + nouns[nounIndex] + "-" + id
 }
